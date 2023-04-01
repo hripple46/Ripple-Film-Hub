ec10f0b49ee26a3efd636e1e1ed4f41bafb66057
Henry Ripple <hripple46@gmail.com>
2023-04-01 18:25:19 -0500

    stars are highlighted when hover over

--- src/Rating.js
+++ src/Rating.js
@@ -9,8 +9,27 @@ export default function Rating(props) {
     fiveStars.style.display = "flex"; //displaying the five stars for rating
     fiveStars.addEventListener("click", () => {
       fiveStars.style.display = "none"; //hiding the five stars after a rating is submitted
+      let star = document.querySelector(".mainFiveStars");
+      let stars = star.getElementsByTagName("svg");
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].style.fill = "white";
+      }
     });
   };
+  let highlight = () => {
+    //highlighting the stars when the mouse is over them
+    let star = document.querySelector(".mainFiveStars");
+    let stars = star.getElementsByTagName("svg");
+    console.log(stars);
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].addEventListener("mouseover", function () {
+        for (let j = 0; j < i + 1; j++) {
+          stars[j].style.fill = "yellow";
+        }
+      });
+    }
+  };
+  highlight();
 
   return (
     <div key={props.key} className="oneStar" id={props.id} onClick={showStars}>
