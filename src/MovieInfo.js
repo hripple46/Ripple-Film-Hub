68d022c91b9863ba77076a4663512578bb20b2b1
Henry Ripple <hripple46@gmail.com>
2023-04-03 15:18:04 -0500

    added function to pull individual movie details on click

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -3,6 +3,37 @@ import { useLocation } from "react-router-dom";
 export default function MovieInfo() {
   let location = useLocation();
   let { title } = location.state;
-
-  return <div>{title}</div>;
+  //convert the title to a string and replace all spaces with a plus sign
+  let titleString = title.toString();
+  let titleStringPlus = titleString.replace(/ /g, "+");
+  console.log(titleStringPlus);
+  fetch(
+    "https://api.themoviedb.org/3/search/movie?api_key=485d8a56998fcd2544afe768df960067&query=" +
+      titleStringPlus
+  )
+    .then(function (resposne) {
+      return resposne.json();
+    })
+    .then(function (results) {
+      return results.results[0].id;
+    })
+    .then(function (id) {
+      let idInteger = parseInt(id);
+      console.log(idInteger);
+      return idInteger;
+    })
+    .then(function (id) {
+      return fetch(
+        "https://api.themoviedb.org/3/movie/" +
+          `${id}` +
+          "?api_key=485d8a56998fcd2544afe768df960067"
+      );
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+  return;
 }
