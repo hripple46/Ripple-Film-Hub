d3198312d16700f44a121477781256ee4a117ffa
Henry Ripple <hripple46@gmail.com>
2023-04-05 22:04:05 -0500

    added styling for a drop down and styling to rating stars

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,16 +3,29 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 
 let NavBar = () => {
+  let showMenu = () => {
+    let menu = document.querySelector(".menu");
+    menu.style.transform = "translate(0)";
+    menu.addEventListener("click", () => {
+      menu.style.transform = "translateY(-100vh)";
+    });
+  };
   return (
-    <div className="navbarContainer">
-      <div className="navbar">
-        <Link className="homepageLink" to="/">
-          <h3 className="appTitle">RIPDb</h3>
-        </Link>
-        <h3>Menu</h3>
-        <input type="text" placeholder="Search..." />
+    <div>
+      <div className="menu">
+        <h3>Trending</h3>
+        <h3>Upcoming</h3>
       </div>
-      <div className="spacer"></div>
+      <div className="navbarContainer">
+        <div className="navbar">
+          <Link className="homepageLink" to="/">
+            <h3 className="appTitle">RIPDb</h3>
+          </Link>
+          <h3 onClick={showMenu}>Menu</h3>
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div className="spacer"></div>
+      </div>
     </div>
   );
 };
