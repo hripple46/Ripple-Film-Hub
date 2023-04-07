2f74c6caa79a71eccb0091e40d6528e20a21f1e1
Henry Ripple <hripple46@gmail.com>
2023-04-07 15:52:13 -0500

    updated package.json for gh-pages

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import App from "./App";
 import MovieInfo from "./MovieInfo";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/movieinfo" element={<MovieInfo />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 export default RouteSwitch;
