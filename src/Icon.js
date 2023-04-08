31491e8c3dc940d07b34f999ace9be91098efaff
Henry Ripple <hripple46@gmail.com>
2023-04-08 15:34:51 -0500

    added menu functionality

--- src/Icon.js
+++ src/Icon.js
@@ -4,6 +4,7 @@ import "./Icon.css";
 function Icon() {
   return (
     <svg
+      className="icon"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsvectornator="http://vectornator.io"
       strokeLinecap="round"
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Search from "./Search";
+import IconX from "./Icon-X";
 
 let NavBar = () => {
   let showMenu = () => {
@@ -10,6 +11,44 @@ let NavBar = () => {
     menu.addEventListener("click", () => {
       menu.style.transform = "translateY(-100%)";
     });
+  };
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  async function scrollToUpcomingAndWait(navigateFunc) {
+    //this function waits for the page to load before scrolling
+    navigateFunc("/");
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, 500)
+    );
+  }
+  function scrollToUpcoming() {
+    //this function scrolls to the upcoming movies section
+    if (location.pathname !== "/") {
+      //if the user is not on the homepage, navigate to the homepage and then scroll
+      scrollToUpcomingAndWait(navigate).then(() => {
+        let upcoming = document.querySelector(".exploreMovies");
+        upcoming.scrollIntoView({ behavior: "smooth" });
+      });
+    } else {
+      let upcoming = document.querySelector(".exploreMovies");
+      upcoming.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+  let scrollToTrending = () => {
+    //this function scrolls to the trending movies section
+    if (location.pathname !== "/") {
+      //if the user is not on the homepage, navigate to the homepage and then scroll
+      scrollToUpcomingAndWait(navigate).then(() => {
+        let trending = document.querySelector(".trendingMovies");
+        trending.scrollIntoView({ behavior: "smooth" });
+      });
+    } else {
+      let trending = document.querySelector(".trendingMovies");
+      trending.scrollIntoView({ behavior: "smooth" });
+    }
   };
   return (
     <div>
@@ -21,8 +60,15 @@ let NavBar = () => {
         <Search />
       </div>
       <div className="menu">
-        <h3>Trending</h3>
-        <h3>Upcoming</h3>
+        <div className="menuTop">
+          <h3 className="appTitle">RIPDb</h3>
+          <div>
+            <IconX />
+          </div>
+        </div>
+
+        <h3 onClick={scrollToTrending}>Trending</h3>
+        <h3 onClick={scrollToUpcoming}>Upcoming</h3>
       </div>
     </div>
   );
